0ab8e226bffccb815a7ca3363dffc2ed453e84f2
antonius1980 <antonius1980@gmail.com>
2025-10-04 04:57:29 +0300

    Updating sections

--- js/script.js
+++ js/script.js
@@ -1,1 +1,43 @@
 'use strict';
+
+				const pushedBtn1 = document.querySelector("#push1");
+				function push1() {pushedBtn1.classList.toggle("out")};
+				pushedBtn1.addEventListener("mouseover", push1);
+
+				const pushedBtn2 = document.querySelector("#push2");
+				function push2() {pushedBtn2.classList.toggle("pushed")};
+				pushedBtn2.addEventListener("click", push2);
+
+$(document).ready(function(){
+  $('.slider').slick({
+    arrows: false,
+    dots: true,
+
+    autoplay: true,
+    autoplaySpeed: 4500,
+    speed: 1500,
+    infinite: true,
+     fade: true,
+
+     responsive: [
+            {
+                breakpoint: 998,
+                settings: {
+                    dots: false,
+                    arrows: true,
+                    slidesToShow: 1,
+                }
+            },
+            {breakpoint: 768,
+                settings: {
+                    dots: false,
+                    arrows: false,
+                    autoplaySpeed: 3000,
+                    speed: 800,
+                }  
+            },
+        ]
+
+  });
+});
+
